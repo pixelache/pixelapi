355b7bc93ea4f61d4bf0166609f8833bbb44a087
John W. Fail <john@ptarmigan.fi>
2021-05-21 13:47:19 +0300

    latest festival themes serializer code

--- app/serializers/contributor_serializer.rb
+++ app/serializers/contributor_serializer.rb
@@ -3,4 +3,7 @@ class ContributorSerializer
   attributes :name, :alphabetical_name, :slug, :bio, :image_url, :website
   has_many :contributor_relations
   has_many :festivalthemes
+  attribute :image_box_url do |obj|
+    obj.image? ? obj.image.url(:box) : nil
+  end
 end
