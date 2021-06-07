ab43213a13acd159f0f050ab51a9776297398db9
John W. Fail <john@ptarmigan.fi>
2021-06-07 19:42:43 +0300

    faster eager loading of festival

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -24,7 +24,7 @@ module Api::V1
       def index
         if params[:festival_id]
           @festival = Festival.friendly.includes(:festivalthemes).find(params[:festival_id])
-          @events = apply_scopes(@festival.events.includes([:festivalthemes, contributor_relations: :contributor], :festivalthemes)).order(:start_at, :slug, :end_at).published
+          @events = apply_scopes(@festival.events.includes([:translations, :contributors, :festival, place: [:translations], festivalthemes: [:translations], contributor_relations: [:relation, :contributor]])).order(:start_at, :slug, :end_at).published
         else
           @events = apply_scopes(Event).published
         end
